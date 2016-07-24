9773c74f3981216f1eb3b5de79153b775bc475fc
Dean Armada <deanarmada@gmail.com>
2016-07-24 19:53:57 +0800

    Added Requirements

--- store/models.py
+++ store/models.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from django.core.urlresolvers import reverse
 from django.db import models
 
+# from django_elasticsearch.models import EsIndexable
+
 # Create your models here.
 class Artist(models.Model):
 	name = models.CharField(max_length=50)
